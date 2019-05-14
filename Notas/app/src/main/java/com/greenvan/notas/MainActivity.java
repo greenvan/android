af821cdfd7ef0e710f97c541ffa3a820ec99ebf6
greenvan <50578539+greenvan@users.noreply.github.com>
2019-05-14 09:09:37 +0200

    Añado lista de notas con una clase especial nota.

--- Notas/app/src/main/java/com/greenvan/notas/MainActivity.java
+++ Notas/app/src/main/java/com/greenvan/notas/MainActivity.java
@@ -2,12 +2,31 @@ package com.greenvan.notas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Nota> notas;
+    private ListView lista_notas;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        notas = new ArrayList<>();
+        notas.add(new Nota("hola","que tal"));
+        notas.add(new Nota("1234","567899"));
+
+        lista_notas = (ListView)findViewById(R.id.lista_notas);
+        lista_notas.setAdapter(new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                notas
+        ));
     }
 }
