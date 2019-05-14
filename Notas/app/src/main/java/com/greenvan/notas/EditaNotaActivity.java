c6fabfa5349d1b78dcfb3eb6520d33fe6434ac88
greenvan <50578539+greenvan@users.noreply.github.com>
2019-05-14 13:19:40 +0200

    Añadimos la actividad EditaNotaActivity para poder editar notas y añadir notas nuevas. Añadimos botón para guardar

---
+++ Notas/app/src/main/java/com/greenvan/notas/EditaNotaActivity.java
@@ -0,0 +1,59 @@
+package com.greenvan.notas;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+public class EditaNotaActivity extends AppCompatActivity {
+
+    private int position = -1;
+    private EditText edit_titulo;
+    private EditText edit_texto;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        Log.i("debugtimegreenvan","Entro en oncreate");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edita_nota);
+
+        edit_titulo = (EditText)findViewById(R.id.edit_titulo);
+        edit_texto = (EditText) findViewById(R.id.edit_texto);
+
+        Intent intent = getIntent();
+
+        if(intent.hasExtra("titulo")){
+            edit_titulo.setText(intent.getStringExtra("titulo"));
+            edit_texto.setText(intent.getStringExtra("texto"));
+            position = intent.getIntExtra("position",-1);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.edita_nota_options,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+       switch (item.getItemId()){
+           case R.id.guardar:
+               Intent data = new Intent();
+               data.putExtra("titulo",edit_titulo.getText().toString());
+               data.putExtra("texto",edit_texto.getText().toString());
+               if (position!=-1) data.putExtra("position",position);
+               setResult(RESULT_OK, data);
+               finish();
+               return true;
+       }
+
+       return super.onOptionsItemSelected(item);
+    }
+}
--- Notas/app/src/main/java/com/greenvan/notas/MainActivity.java
+++ Notas/app/src/main/java/com/greenvan/notas/MainActivity.java
@@ -1,12 +1,15 @@
 package com.greenvan.notas;
 
 import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -15,6 +18,8 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int NUEVA_NOTA = 0;
+    public static final int EDITA_NOTA = 1;
     private NotasAdapter adapter;
     private ArrayList<Nota> notas;
     private ListView lista_notas;
@@ -35,8 +40,64 @@ public class MainActivity extends AppCompatActivity {
         lista_notas.setAdapter(new ArrayAdapter<Nota>(this,android.R.layout.simple_list_item_1,notas));
 
         lista_notas.setAdapter(adapter);
+
+
+        lista_notas.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                onEditaNota(position);
+            }
+        });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        if (resultCode==RESULT_OK){
+
+            String titulo = data.getStringExtra("titulo");
+            String texto = data.getStringExtra("texto");
+            int position = data.getIntExtra("position",-1);
+
+
+
+            switch (requestCode){
+                case NUEVA_NOTA:
+                    Nota nueva_nota = new Nota(titulo,texto);
+                    notas.add(nueva_nota);
+                    adapter.notifyDataSetChanged();
+                    break;
+                case EDITA_NOTA:
+                    Nota nota = notas.get(position);
+                    nota.setTitulo(titulo);
+                    nota.setTexto(texto);
+                    adapter.notifyDataSetChanged();
+                    break;
+                default:
+                    super.onActivityResult(requestCode, resultCode, data);
+            }
+
+        }
+
+
+
+    }
+
+    private void onEditaNota(int position) {
+        Nota nota = notas.get(position);
+        Intent intent = new Intent(this,EditaNotaActivity.class);
+        intent.putExtra("titulo", nota.getTitulo());
+        intent.putExtra("texto",nota.getTexto());
+        intent.putExtra("position", position);
+        startActivityForResult(intent, EDITA_NOTA);
+
+    }
+
+    public void onNuevaNota(View view) {
+        Intent intent = new Intent(this, EditaNotaActivity.class);
+        startActivityForResult(intent, NUEVA_NOTA);
+
+    }
 
 
     private class NotasAdapter extends ArrayAdapter<Nota>{
